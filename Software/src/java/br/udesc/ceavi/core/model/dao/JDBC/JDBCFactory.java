c8c3c0a9787bd1a763badd89c70fcc34312c251e
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2016-06-19 18:51:15 -0300

    re-inclusão da factory dos daos do produto
    inclusao dos métodos do produto dao

--- Software/src/java/br/udesc/ceavi/core/model/dao/JDBC/JDBCFactory.java
+++ Software/src/java/br/udesc/ceavi/core/model/dao/JDBC/JDBCFactory.java
@@ -1,6 +1,6 @@
 package br.udesc.ceavi.core.model.dao.JDBC;
+
 import br.udesc.ceavi.caixeiro.model.dao.iDaoCentroDistribuicao;
-import br.udesc.ceavi.caixeiro.model.dao.iDaoCesta;
 import br.udesc.ceavi.caixeiro.model.dao.iDaoCliente;
 import br.udesc.ceavi.caixeiro.model.dao.iDaoEndereco;
 import br.udesc.ceavi.caixeiro.model.dao.iDaoEntrega;
@@ -11,13 +11,24 @@ import br.udesc.ceavi.caixeiro.model.dao.JDBC.JDBCDAOCentroDistribuicao;
 import br.udesc.ceavi.caixeiro.model.dao.JDBC.JDBCDAOCliente;
 import br.udesc.ceavi.caixeiro.model.dao.JDBC.JDBCDAOEndereco;
 import br.udesc.ceavi.caixeiro.model.dao.JDBC.JDBCDAOEntrega;
-import br.udesc.ceavi.caixeiro.model.dao.JDBC.JDBCDAOCesta;
 import br.udesc.ceavi.caixeiro.model.dao.JDBC.JDBCDAOFornecedor;
 import br.udesc.ceavi.caixeiro.model.dao.JDBC.JDBCDAORelacionamentoEndereco;
 import br.udesc.ceavi.caixeiro.model.dao.JDBC.JDBCDAOVeiculo;
 import br.udesc.ceavi.core.persistence.Persistence;
 import br.udesc.ceavi.core.persistence.Persistible;
 import br.udesc.ceavi.core.util.ClassUtils;
+import br.udesc.ceavi.produto.model.dao.categoria.CategoriaDAO;
+import br.udesc.ceavi.produto.model.dao.categoria.JDBCCategoriaDAO;
+import br.udesc.ceavi.produto.model.dao.cesta.CestaDAO;
+import br.udesc.ceavi.produto.model.dao.cesta.JDBCCestaDAO;
+import br.udesc.ceavi.produto.model.dao.cestaproduto.CestaProdutoDAO;
+import br.udesc.ceavi.produto.model.dao.cestaproduto.JDBCCestaProdutoDAO;
+import br.udesc.ceavi.produto.model.dao.clienteproduto.ClienteProdutoDAO;
+import br.udesc.ceavi.produto.model.dao.clienteproduto.JDBCClienteProdutoDAO;
+import br.udesc.ceavi.produto.model.dao.produto.JDBCProdutoDAO;
+import br.udesc.ceavi.produto.model.dao.produto.ProdutoDAO;
+import br.udesc.ceavi.produto.model.fornecedorproduto.FornecedorProdutoDAO;
+import br.udesc.ceavi.produto.model.fornecedorproduto.JDBCFornecedorProdutoDAO;
 import java.util.HashMap;
 
 /**
@@ -27,51 +38,82 @@ import java.util.HashMap;
  */
 public class JDBCFactory extends Persistence {
 
-        private static HashMap<String, Persistible> daos = new HashMap<>();
+    private static HashMap<String, Persistible> daos = new HashMap<>();
 
-	public static iDaoCentroDistribuicao getDaoCentroDistribuicao() {
-            return (iDaoCentroDistribuicao) load(JDBCDAOCentroDistribuicao.class);
-	}
+    public static iDaoCentroDistribuicao getDaoCentroDistribuicao() {
+        return (iDaoCentroDistribuicao) load(JDBCDAOCentroDistribuicao.class);
+    }
 
-	public static iDaoCesta getDaoCesta() {
-            return (iDaoCesta) load(JDBCDAOCesta.class);
-	}
+//    public static iDaoCesta getDaoCesta() {
+//        return (iDaoCesta) load(JDBCDAOCesta.class);
+//    } não tem daocesta ainda
 
-	public static iDaoCliente getDaoCliente(){
-            return (iDaoCliente) load(JDBCDAOCliente.class);
-	}
+    public static iDaoCliente getDaoCliente() {
+        return (iDaoCliente) load(JDBCDAOCliente.class);
+    }
 
-	public static iDaoEndereco getDaoEndereco() {
-            return (iDaoEndereco) load(JDBCDAOEndereco.class);
-	}
+    public static iDaoEndereco getDaoEndereco() {
+        return (iDaoEndereco) load(JDBCDAOEndereco.class);
+    }
 
-	public static iDaoEntrega getDaoEntrega(){
-            return (iDaoEntrega) load(JDBCDAOEntrega.class);
-	}
+    public static iDaoEntrega getDaoEntrega() {
+        return (iDaoEntrega) load(JDBCDAOEntrega.class);
+    }
 
-	public static iDaoVeiculo getDaoVeiculo(){
-            return (iDaoVeiculo) load(JDBCDAOVeiculo.class);
-	}
+    public static iDaoVeiculo getDaoVeiculo() {
+        return (iDaoVeiculo) load(JDBCDAOVeiculo.class);
+    }
 
-	public static iDaoRelacionamentoEndereco getDaoRelacionamentoEndereco(){
-            return (iDaoRelacionamentoEndereco) load(JDBCDAORelacionamentoEndereco.class);
-	}
+    public static iDaoRelacionamentoEndereco getDaoRelacionamentoEndereco() {
+        return (iDaoRelacionamentoEndereco) load(JDBCDAORelacionamentoEndereco.class);
+    }
 
-        public static iDaoFornecedor getDaoFornecedor(){
-            return (iDaoFornecedor) load(JDBCDAOFornecedor.class);
-	}
+    public static iDaoFornecedor getDaoFornecedor() {
+        return (iDaoFornecedor) load(JDBCDAOFornecedor.class);
+    }
 
-        /**
-         * Load an instance of the required class
-         * @param aClass
-         * @return
-         */
-        private static Persistible load(Class<? extends Persistible> aClass) {
-            if(!daos.containsKey(aClass.getName())) {
-                daos.put(aClass.getName(), ClassUtils.getNewPOJO(aClass));
-            }
+    @Override
+    public CestaDAO getCestaDAO() {
+        return new JDBCCestaDAO();
+    }
 
-            return daos.get(aClass.getName());
+    @Override
+    public CategoriaDAO getCategoriaDAO() {
+        return new JDBCCategoriaDAO();
+    }
+
+    @Override
+    public ProdutoDAO getProdutoDAO() {
+        return new JDBCProdutoDAO();
+    }
+
+    @Override
+    public ClienteProdutoDAO getClienteProdutoDAO() {
+        return new JDBCClienteProdutoDAO();
+    }
+
+    @Override
+    public FornecedorProdutoDAO getFornecedorProdutoDAO() {
+        return new JDBCFornecedorProdutoDAO();
+    }
+
+    @Override
+    public CestaProdutoDAO getCestaProdutoDAO() {
+        return new JDBCCestaProdutoDAO();
+    }
+
+    /**
+     * Load an instance of the required class
+     *
+     * @param aClass
+     * @return
+     */
+    private static Persistible load(Class<? extends Persistible> aClass) {
+        if (!daos.containsKey(aClass.getName())) {
+            daos.put(aClass.getName(), ClassUtils.getNewPOJO(aClass));
         }
+
+        return daos.get(aClass.getName());
+    }
 
 }//end JDBCFactory
