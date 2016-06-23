a0345595565b741356f6d61adb89b32b6ace8ceb
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:26:57 -0300

    Update Produto.java

--- Software/src/java/br/udesc/ceavi/produto/model/entidade/Produto.java
+++ Software/src/java/br/udesc/ceavi/produto/model/entidade/Produto.java
@@ -2,15 +2,6 @@ package br.udesc.ceavi.produto.model.entidade;
 
 import br.udesc.ceavi.produto.model.entidade.Categoria;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author ignoi
- */
 public class Produto {
 
     private int id;
