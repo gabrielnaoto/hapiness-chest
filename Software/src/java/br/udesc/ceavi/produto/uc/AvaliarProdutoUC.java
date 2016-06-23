ae2ccc3c361435fee39358f25ef31314cec17c24
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:20:41 -0300

    Update AvaliarProdutoUC.java

--- Software/src/java/br/udesc/ceavi/produto/uc/AvaliarProdutoUC.java
+++ Software/src/java/br/udesc/ceavi/produto/uc/AvaliarProdutoUC.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.uc;
 
 import br.udesc.ceavi.caixeiro.model.Usuario;
@@ -21,7 +16,7 @@ import java.util.List;
 - O cliente pode avaliar o produto quantas vezes for necessário (desde que esteja dentro do prazo) e apenas a ultima avaliaçao ficara salva.
 
 - Os produtos devem entrar para avaliaçao assim que forem lançados pelos fornecedores.
- * @author ignoi
+ *
  */
 public class AvaliarProdutoUC {
     
