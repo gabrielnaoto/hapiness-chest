efa8c33878e33a747bdc979e513d540d1c009266
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:33:32 -0300

    Update FornecedorProdutoDAO.java

--- Software/src/java/br/udesc/ceavi/produto/model/dao/fornecedorproduto/FornecedorProdutoDAO.java
+++ Software/src/java/br/udesc/ceavi/produto/model/dao/fornecedorproduto/FornecedorProdutoDAO.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.model.dao.fornecedorproduto;
 
 import br.udesc.ceavi.produto.model.entidade.FornecedorProduto;
 import java.util.List;
 
-/**
- *
- * @author ignoi
- */
 public interface FornecedorProdutoDAO {
 
     public boolean inserir(FornecedorProduto c);
