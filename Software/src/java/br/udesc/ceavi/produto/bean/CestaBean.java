2990f73f2e8de034780a38a2e71985b609d7c5b2
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:19:21 -0300

    Update CestaBean.java

--- Software/src/java/br/udesc/ceavi/produto/bean/CestaBean.java
+++ Software/src/java/br/udesc/ceavi/produto/bean/CestaBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.bean;
 
 import br.udesc.ceavi.produto.model.entidade.Categoria;
@@ -22,10 +17,6 @@ import javax.faces.context.FacesContext;
 import org.primefaces.context.RequestContext;
 import org.primefaces.event.SelectEvent;
 
-/**
- *
- * @author ignoi
- */
 @ManagedBean
 @ViewScoped
 public class CestaBean implements Serializable {
