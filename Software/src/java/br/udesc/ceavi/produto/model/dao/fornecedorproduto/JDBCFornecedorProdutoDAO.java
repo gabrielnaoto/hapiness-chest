0513cb1b34f1e01bc0ec32513bd298c90678aac4
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:33:13 -0300

    Update JDBCFornecedorProdutoDAO.java

--- Software/src/java/br/udesc/ceavi/produto/model/dao/fornecedorproduto/JDBCFornecedorProdutoDAO.java
+++ Software/src/java/br/udesc/ceavi/produto/model/dao/fornecedorproduto/JDBCFornecedorProdutoDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.model.dao.fornecedorproduto;
 
 import br.udesc.ceavi.produto.model.entidade.FornecedorProduto;
@@ -11,10 +6,6 @@ import java.sql.PreparedStatement;
 import java.util.Calendar;
 import java.util.List;
 
-/**
- *
- * @author Sila Siebert
- */
 public class JDBCFornecedorProdutoDAO implements FornecedorProdutoDAO {
 
     @Override
