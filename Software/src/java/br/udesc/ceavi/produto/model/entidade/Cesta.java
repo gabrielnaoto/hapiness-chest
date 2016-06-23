0d7a1362b79975644662965bfbeaa096f593f5df
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:24:33 -0300

    Update Cesta.java

--- Software/src/java/br/udesc/ceavi/produto/model/entidade/Cesta.java
+++ Software/src/java/br/udesc/ceavi/produto/model/entidade/Cesta.java
@@ -1,10 +1,5 @@
 package br.udesc.ceavi.produto.model.entidade;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import br.udesc.ceavi.produto.model.entidade.state.CestaEmAndamento;
 import br.udesc.ceavi.produto.model.entidade.state.CestaState;
 import java.io.Serializable;
@@ -12,10 +7,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-/**
- *
- * @author ignoi
- */
 public class Cesta implements Serializable, SampleEntity {
 
     private int id;
