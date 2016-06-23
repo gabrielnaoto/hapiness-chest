b7454ef319db0566ca0ea7a33abee0a53d1f4dc4
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:32:47 -0300

    Update ClienteProdutoDAO.java

--- Software/src/java/br/udesc/ceavi/produto/model/dao/clienteproduto/ClienteProdutoDAO.java
+++ Software/src/java/br/udesc/ceavi/produto/model/dao/clienteproduto/ClienteProdutoDAO.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.model.dao.clienteproduto;
 
 import br.udesc.ceavi.produto.model.entidade.ClienteProduto;
 import java.util.List;
 
-/**
- *
- * @author ignoi
- */
 public interface ClienteProdutoDAO {
 
     public boolean inserir(ClienteProduto c);
