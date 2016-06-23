98a4f79db4f9f544cf4ce9826aaa1e065bd0c68d
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:22:42 -0300

    Update Conexao.java

--- Software/src/java/br/udesc/ceavi/produto/util/Conexao.java
+++ Software/src/java/br/udesc/ceavi/produto/util/Conexao.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author ignoi
- */
 public class Conexao {
 
     public static class Database {
