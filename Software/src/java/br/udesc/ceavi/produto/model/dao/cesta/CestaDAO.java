74e6abb4b3f63c69463ccc9135d2f49d247f82ae
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:29:51 -0300

    Update CestaDAO.java

--- Software/src/java/br/udesc/ceavi/produto/model/dao/cesta/CestaDAO.java
+++ Software/src/java/br/udesc/ceavi/produto/model/dao/cesta/CestaDAO.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.model.dao.cesta;
 
 import br.udesc.ceavi.produto.model.entidade.Cesta;
 import java.util.List;
 
-/**
- *
- * @author ignoi
- */
 public interface CestaDAO {
 
     public boolean inserir(Cesta c);
