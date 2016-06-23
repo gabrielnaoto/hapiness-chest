a9c9853cdf4108f578ff3270e23e801ee60316ae
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:24:16 -0300

    Update Categoria.java

--- Software/src/java/br/udesc/ceavi/produto/model/entidade/Categoria.java
+++ Software/src/java/br/udesc/ceavi/produto/model/entidade/Categoria.java
@@ -1,14 +1,5 @@
 package br.udesc.ceavi.produto.model.entidade;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author ignoi
- */
 public class Categoria {
 
     private int id;
