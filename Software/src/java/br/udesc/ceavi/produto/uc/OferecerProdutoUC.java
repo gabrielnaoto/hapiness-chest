e11e5a9cd362df195ecfb2c417904a25f23babe9
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:21:39 -0300

    Update OferecerProdutoUC.java

--- Software/src/java/br/udesc/ceavi/produto/uc/OferecerProdutoUC.java
+++ Software/src/java/br/udesc/ceavi/produto/uc/OferecerProdutoUC.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.uc;
 
 import br.udesc.ceavi.caixeiro.model.Usuario;
@@ -25,7 +20,6 @@ import java.util.List;
  * - Para calculo da cesta, os valores dos produtos lancados devem ser
  * arredondados pra cima e serem tratados como inteiro
  *
- * @author ignoi
  */
 public class OferecerProdutoUC {
 
