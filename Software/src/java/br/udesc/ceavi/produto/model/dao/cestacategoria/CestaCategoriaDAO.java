f4ee6dba205c1a89faa0f76b0ec7495a4a434301
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:30:47 -0300

    Update CestaCategoriaDAO.java

--- Software/src/java/br/udesc/ceavi/produto/model/dao/cestacategoria/CestaCategoriaDAO.java
+++ Software/src/java/br/udesc/ceavi/produto/model/dao/cestacategoria/CestaCategoriaDAO.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.model.dao.cestacategoria;
 
 import java.util.List;
 import br.udesc.ceavi.produto.model.entidade.CestaCategoria;
 
-/**
- *
- * @author ignoi
- */
 public interface CestaCategoriaDAO {
 
     public boolean inserir(CestaCategoria c);
