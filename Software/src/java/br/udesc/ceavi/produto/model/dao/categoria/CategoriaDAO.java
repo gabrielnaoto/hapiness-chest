a12e143e4ef5acd0edc5a89d55aa6b706af099fc
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:29:25 -0300

    Update CategoriaDAO.java

--- Software/src/java/br/udesc/ceavi/produto/model/dao/categoria/CategoriaDAO.java
+++ Software/src/java/br/udesc/ceavi/produto/model/dao/categoria/CategoriaDAO.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.model.dao.categoria;
 
 import br.udesc.ceavi.produto.model.entidade.Categoria;
 import java.util.List;
 
-/**
- *
- * @author ignoi
- */
 public interface CategoriaDAO {
 
     public boolean inserir(Categoria c);
