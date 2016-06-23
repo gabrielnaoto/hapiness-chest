c400401b183ece169da182fe6a5adc6dd5e97db3
Luma Hoscher <luhoscher@gmail.com>
2016-06-23 00:32:18 -0300

    Update JDBCClienteProdutoDAO.java

--- Software/src/java/br/udesc/ceavi/produto/model/dao/clienteproduto/JDBCClienteProdutoDAO.java
+++ Software/src/java/br/udesc/ceavi/produto/model/dao/clienteproduto/JDBCClienteProdutoDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.udesc.ceavi.produto.model.dao.clienteproduto;
 
 import br.udesc.ceavi.caixeiro.model.Cliente;
@@ -20,10 +15,6 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Sila Siebert
- */
 public class JDBCClienteProdutoDAO implements ClienteProdutoDAO {
 
     @Override
